6118712610d03a6f99ecf7b385f9f94bdc1a8cb9
sudr <to_sud@yahoo.com>
2014-05-07 06:18:14 -0500

    if else

--- src/1-starting-out.hs
+++ src/1-starting-out.hs
@@ -1,1 +1,13 @@
 doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+-- Now we're going to make a function that multiplies a number by 2 but only if that number is smaller than or equal to 100
+
+doubleSmallNumber x = if x > 100 
+                        then x
+                        else doubleMe x
+
+-- an if statement always returns an expression; therefore else is mandatory     
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
